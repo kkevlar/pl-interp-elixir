24901ca8cbe10e9c8d3a57290c69ef96ce499a59
Kevin Kellar <kellar.kevin@gmail.com>
2020-03-11 09:30:26 -0700

    if works

--- example/lib/interp.ex
+++ example/lib/interp.ex
@@ -5,5 +5,18 @@ defmodule Interp do
   def interp(%{str: s}) do
     s
   end
+  def interp(%{id: id}) do
+    cond do
+      id == :true -> true
+      id == :false -> false
+    end
+  end
+  def interp(%{test: test, then: then, else: els}) do
+    if Interp.interp(test) do
+      Interp.interp(then)
+    else
+      Interp.interp(els)
+    end
+  end
 end
 
--- example/test/interp_test.exs
+++ example/test/interp_test.exs
@@ -12,5 +12,31 @@ defmodule StructmeTest do
     expr = %ExprC.StringC{str: "hello"}
     assert Interp.interp(expr) == "hello" 
   end
+  test "interp stupid id true" do
+    bool = %ExprC.IdentifierC{id: :true}
+    assert Interp.interp(bool) == true
+  end
+  test "interp stupid id false" do
+    bool = %ExprC.IdentifierC{id: :false}
+    assert Interp.interp(bool) == false
+  end
+  test "interp an if true" do
+    etest = %ExprC.IdentifierC{id: :true}
+    ethen = %ExprC.NumC{num: 42} 
+    eelse = %ExprC.IdentifierC{id: :true}
+
+    expr = %ExprC.IfC{test: etest, then: ethen, else: eelse}
+    assert Interp.interp(expr) == 42
+  end
+  test "interp an if false" do
+    etest = %ExprC.IdentifierC{id: :false}
+    ethen = %ExprC.NumC{num: 42} 
+    eelse = %ExprC.IdentifierC{id: :true}
+
+    expr = %ExprC.IfC{test: etest, then: ethen, else: eelse}
+    assert Interp.interp(expr) == true
+  end
+  
+
 end
 
