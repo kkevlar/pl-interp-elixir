25ca3b7c9bf39d665df90ee1cb35544bf5bac6d0
Kevin Kellar <kellar.kevin@gmail.com>
2020-03-11 09:12:52 -0700

    added interp modules

---
+++ example/test/interp_test.exs
@@ -0,0 +1,45 @@
+defmodule StructmeTest do
+  use ExUnit.Case
+
+  test "sanity" do
+    assert 1 == 1
+  end
+  test "makenum" do
+    eight = %ExprC.NumC{num: 8}
+    oof = eight.num
+    assert oof == 8
+  end
+  test "makestr" do
+    lmao = %ExprC.StringC{str: "lmao"}
+    oof = lmao.str
+    assert oof == "lmao" 
+  end
+  test "makeif" do
+    if = %ExprC.IfC{test: false, then: 20, else: "yay"}
+    test = if.test 
+    then = if.then 
+    els = if.else 
+    assert test == false
+    assert els == "yay"
+    assert then == 20
+  end
+  test "makeif with ast" do
+    eight = %ExprC.NumC{num: 8}
+    if = %ExprC.IfC{test: false, then: eight, else: eight}
+    test = if.test 
+    then = if.then 
+    els = if.else 
+    assert test == false
+    assert els.num == 8
+    assert then.num == 8
+  end
+  test "identifier make" do
+    theid = %ExprC.IdentifierC{id: :lmao}
+    assert theid.id == :lmao
+  end
+  test "identifier make +" do
+    theid = %ExprC.IdentifierC{id: :+}
+    assert theid.id == :+
+  end
+end
+
