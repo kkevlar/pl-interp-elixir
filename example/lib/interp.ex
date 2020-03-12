d79ada1d46cdf63cc84f017a83d62b55d064b649
FelixSitu <chsitu@calpoly.edu>
2020-03-12 00:39:15 -0700

    Added comments to interp.ex for each exprC

--- example/lib/interp.ex
+++ example/lib/interp.ex
@@ -1,10 +1,13 @@
 defmodule Interp do
+  #NumC
   def interp(%{num: n}, _env) do
     n
   end
+  #StringC
   def interp(%{str: s}, _env) do
     s
   end
+  #IdC
   def interp(%{id: id}, _env) do
     cond do
       id == :true -> true
@@ -12,6 +15,8 @@ defmodule Interp do
     end
     #Lookup.lookup(id, _env)
   end
+
+  #IfC
   def interp(%{test: test, then: then, else: els}, env) do
     if Interp.interp(test, env) do
       Interp.interp(then, env)
@@ -19,4 +24,8 @@ defmodule Interp do
       Interp.interp(els, env)
     end
   end
+
+  #LamC
+
+  #AppC
 end
